11701b2ba168e5424d3a7660af1a8c46212315c9
xiaozhongbao <1239851729@qq.com>
2016-03-03 15:40:34 +0800

    实现hello xzhb

---
+++ src/main/java/controller/Hello.java
@@ -0,0 +1,30 @@
+import java.lang.Override;
+import java.lang.String;
+import java.lang.System;
+
+
+public class Hello{
+    private String name;
+    public void sayHello(){
+
+
+        System.out.println("hello "+name);
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Hello{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
